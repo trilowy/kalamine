62d3307e8d9d2da1a06733107da9617d9f9a7cde
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-12-22 00:37:41 +0100

    refactor(zig): better CLI

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const ArgParser = @import("cli/ArgParser.zig");
 const cli = @import("cli/cli.zig");
 const cli_help = @import("cli/cli_help.zig");
 const build = @import("command/build.zig");
@@ -7,29 +8,44 @@ const watch = @import("command/watch.zig");
 const version = @import("command/version.zig");
 const CliError = @import("cli/error.zig").CliError;
 
-pub fn main() void {
-    execute() catch {
-        std.process.exit(1);
-    };
-}
-
-fn execute() !void {
+pub fn main() u8 {
     var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
     const allocator = debug_allocator.allocator();
     defer _ = debug_allocator.deinit();
 
-    var stdout_buffer: [1024]u8 = undefined;
+    // Parsing program args
+    const args = std.process.argsAlloc(allocator) catch @panic("Out of memory");
+    defer std.process.argsFree(allocator, args);
+
+    var stdout_buffer: [std.heap.page_size_min]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
     const stdout = &stdout_writer.interface;
 
-    var stderr_buffer: [1024]u8 = undefined;
+    var stderr_buffer: [std.heap.page_size_min]u8 = undefined;
     var stderr_writer = std.fs.File.stderr().writer(&stderr_buffer);
     const stderr = &stderr_writer.interface;
 
-    // Parsing program args
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+    execute(allocator, args, stdout, stderr) catch |err|
+        switch (err) {
+            error.ErrorReported => return 1,
+            else => {
+                std.log.err("{s}", .{@errorName(err)}); // TODO: see what it does, stderr?
+                return 1;
+            },
+        };
 
+    return 0;
+}
+
+fn execute(
+    allocator: std.mem.Allocator,
+    args: []const []const u8,
+    stdout: *std.Io.Writer,
+    stderr: *std.Io.Writer,
+) !void {
+    var arg_parser = ArgParser{ .args = args };
+
+    // TODO: https://github.com/Hejsil/dipm/blob/4b24131e2b010ef8577fb650ca39d2305e6b3b4e/src/main.zig#L73
     const cmd = cli.parse(args) catch |err| {
         switch (err) {
             CliError.MissingArg => {
@@ -83,6 +99,20 @@ fn execute() !void {
         },
     }
 }
+
+const main_usage =
+    \\Usage: kalamine [command] [options]
+    \\
+    \\Commands:
+    \\  build <file> [options]  Convert TOML/YAML descriptions into OS-specific keyboard drivers.
+    \\  new <file> [options]    Create a new TOML layout description.
+    \\  watch <file> [options]  Watch a layout description file and display it in a web browser.
+    \\
+    \\Options:
+    \\  -h --help               Show this screen.
+    \\  --version               Show version.
+    \\
+;
 
 // Run all tests of all modules
 test {
