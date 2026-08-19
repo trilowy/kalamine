b8b80c016f036880ad60a851155ace63fc61a6bb
Trilowy <49493635+trilowy@users.noreply.github.com>
2026-08-19 19:26:30 +0200

    refactor(zig): nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+# On NixOS, install direnv and nix-direnv to auto load the flake when entering the project directory
+{
+  description = "Zig project";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        zig_0_15
+        zls_0_15 # Zig LSP
+      ];
+    };
+  };
+}
